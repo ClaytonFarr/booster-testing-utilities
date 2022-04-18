85e2fb21ab08860f8b95dd4058e71471539d1b67
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-04-18 10:21:44 -0600

    update notes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // threads: false, // disabling parallel tests to avoid this issue for now: https://github.com/vitest-dev/vitest/issues/317#issuecomment-1058106323
     exclude: [...configDefaults.exclude, '.booster/*'],
     watchIgnore: [...configDefaults.watchIgnore, '.booster/*'],
     coverage: {
@@ -12,5 +11,6 @@ export default defineConfig({
     },
     globalSetup: ['test/globalSetup.ts'], // will run before/after *all* tests
     // setupFiles: ['test/setup.ts'], // will run before *each* test file
+    // threads: false, // disabling parallel tests to avoid this issue for now: https://github.com/vitest-dev/vitest/issues/317#issuecomment-1058106323
   },
 })
